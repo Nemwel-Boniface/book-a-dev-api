0052f50b80a5383f339f40588c78681ae6be0636
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-12 14:22:36 +0300

    Update wrote the developers index API endpoints to handle success and errors

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::DevelopersController < ApplicationController
   def index
-    @developer = Developer.all 
-    render json: @developer
+    @developers = Developer.all 
+    
+    if @developers
+      render json: {status: "SUCCESS", message: "Retrieved all the developers correctly!", data: @developers}, status: :ok
+    else
+      render json: @developers.errors, status: :bad_request
+    end
   end
 end
