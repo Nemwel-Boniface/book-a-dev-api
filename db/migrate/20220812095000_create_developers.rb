03550ba17a4950298580f2e3aa1c9d530e972fc9
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-08-12 12:52:14 +0300

    Update created developer model

---
+++ db/migrate/20220812095000_create_developers.rb
@@ -0,0 +1,16 @@
+class CreateDevelopers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :developers do |t|
+      t.string :name
+      t.string :icon
+      t.string :location
+      t.string :email
+      t.integer :experience
+      t.text :bio
+      t.integer :hourly_rate
+      t.string :tech_stack
+
+      t.timestamps
+    end
+  end
+end
