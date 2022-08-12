630a3e4403941d9c2460f9b5d483e89f828769f3
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-08-12 15:20:11 +0300

    Update added show action

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -19,6 +19,16 @@ class Api::V1::DevelopersController < ApplicationController
     end
   end
 
+  def show
+    developer = Developer.find(params[:id])
+
+    if developer
+      render json: {data: developer}, status: :ok 
+    else
+      render json: {message: "Developer cannot be found!"}, status: :bad_request
+    end
+  end
+
   private
 
   def developers_params
