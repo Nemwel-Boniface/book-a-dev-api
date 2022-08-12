84f0dac7c0f8770605b8b3b365b28353abed6f6d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-12 14:15:35 +0300

    Update created starting data in the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Developer.create!([
+  {name: "Nemwel Boniface", icon: "https://images.unsplash.com/photo-1531384441138-2736e62e0919?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", location: "Nairobi, Kenya", email: "nemwelboniface@outlook.com", experience: 2, bio: "Some long text", hourly_rate: 25, tech_stack: ["React", "Rails"]},
+  {name: "Isaac Lumato", icon: "https://images.unsplash.com/photo-1617244147030-8bd6f9e21d1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YmxhY2slMjBtYW4lMjBzbWlsaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60", location: "Dar Es saalam", email: "isakawilly10@gmail.com", experience: 4, bio: "Some very long text", hourly_rate: 35, tech_stack: ["Angular", "Django"]}
+])
