81b5116694309faa27c9da399d5a2aa2d8c46d72
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-12 15:07:35 +0300

    Update included validations for the developers model

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,10 @@
 class Developer < ApplicationRecord
+  validates :name, presence: true
+  validates :icon, presence: true
+  validates :location, presence: true
+  validates :email, presence: true
+  validates :experience, numericality: {only_integer: true, greater_than_or_equal_to: 0}, presence: true
+  validates :bio, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 0}, presence: true
+  validates :tech_stack, presence: true
 end
