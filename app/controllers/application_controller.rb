7358953be9ba91ef2f11b6d9d7da45d698abe0d0
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-15 20:12:40 +0300

    Update checked and fixed rubocop offences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name])
   end
 end
