95ca80fa1300183d24157ebb32fa668342ae7669
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-08-16 20:52:52 +0300

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
     can :read, Developer, available: true
-    #
     return unless user.present?
+
     #   return unless user.admin?
     can :manage, :all
     #
