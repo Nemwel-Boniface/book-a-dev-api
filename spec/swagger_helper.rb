9f7f0630aa9c5a97d6fabb3c3b88bcc4b3880423
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-20 12:36:19 +0300

    Update changed root url from HTTPS to HTTP

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -22,7 +22,7 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
               default: '127.0.0.1:3000/'
