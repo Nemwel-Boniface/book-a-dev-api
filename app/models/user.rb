a8c1168b819c9da42ae25f1adc481c1b6d162dbd
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-23 16:35:58 +0300

    Update included:   - Included a users_controller to the project adding the login and create methods   - Updated the users model validations   - Updated the appliation controller to not use the devise_auth for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ActiveRecord::Base
-  extend Devise::Models
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  include DeviseTokenAuth::Concerns::User
-
   validates :name, presence: true
   validates :email, presence: true
   validates :username, presence: true
+  validates :password, presence: true
 
   has_many :reservations
   has_many :developers
