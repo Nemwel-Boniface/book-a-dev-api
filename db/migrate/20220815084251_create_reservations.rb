f14c6cd49badd7f0c8a7794e1dbd589e22fba730
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-24 12:42:00 +0300

    Update to reservation migration

--- db/migrate/20220815084251_create_reservations.rb
+++ db/migrate/20220815084251_create_reservations.rb
@@ -5,6 +5,7 @@ class CreateReservations < ActiveRecord::Migration[7.0]
       t.datetime :end_date
       t.string :location
       t.references :user, null: false, foreign_key: true
+      t.references :developer, null: false, foreign_key: true
 
       t.timestamps
     end
